93f62dc4e8e8b493a14a8cbb92403255249a99ca
Rafa Laguna <rafa.laguna@enti.cat>
2025-03-06 13:12:02 +0100

    LocalStorage

--- rollmania/src/Scores.js
+++ rollmania/src/Scores.js
@@ -1,14 +1,24 @@
 //import './Scores.css';
 import ScoreItem from './ScoreItem.js';
 
-function Scores(props) {
-	let score_list = [
-		<ScoreItem player_name="Jacinto" player_score="44" key="0" />,
-		<ScoreItem player_name="Trump" player_score="32" key="1" />,
-		<ScoreItem player_name="Anselmo" player_score="43" key="2" />,
-		<ScoreItem player_name="Jacinto" player_score="10" key="3" />
-	];
 
+
+
+
+function Scores(props) {
+	let scores_json = localStorage.getItem("scores");
+
+	let score_list = [];
+
+	let scores_parsed = JSON.parse(scores_json);
+
+	for (let i = 0; i < scores_parsed.length; i++){
+		let name = scores_parsed[i].nombre;
+		let score = scores_parsed[i].score;
+
+		score_list.push(<ScoreItem player_name={name} player_score={score} key={i} />);
+
+	}
 
   return (
     <section className="Scores">
